1867d500188268be9b9ae62ddfaaa849d301fbf8
李智慧 <lizhihui0215@gmail.com>
2017-08-25 19:06:03 +0800

    refine code

--- camile-web/src/main/java/com/camile/web/controller/LoginController.java
+++ camile-web/src/main/java/com/camile/web/controller/LoginController.java
@@ -1,14 +1,32 @@
 package com.camile.web.controller;
 
+import com.camile.api.CmUserService;
 import com.camile.common.base.Controller;
+import com.camile.dao.model.CmUser;
+import com.camile.dao.model.CmUserExample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping(value = "/login")
 public class LoginController extends Controller {
     private static Logger _log = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private CmUserService userService;
+
+    @RequestMapping("/test")
+    public List<CmUser> test() {
+        CmUserExample cmUserExample = new CmUserExample();
+        List<CmUser> cmUsers = this.userService.selectByExample(cmUserExample);
+
+        return cmUsers;
+    }
 
 }
