287a000b7c6212f6b78c0b32057aac2c2dac678c
李智慧 <lizhihui0215@gmail.com>
2017-09-30 18:29:19 +0800

    refine code

--- camile-web/src/main/java/com/camile/web/controller/LoginController.java
+++ camile-web/src/main/java/com/camile/web/controller/LoginController.java
@@ -2,12 +2,14 @@ package com.camile.web.controller;
 
 import com.camile.api.CmUserService;
 import com.camile.common.base.Controller;
+import com.camile.common.base.Response;
 import com.camile.dao.model.CmUser;
 import com.camile.dao.model.CmUserExample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +23,25 @@ public class LoginController extends Controller {
     @Autowired
     private CmUserService userService;
 
-    @RequestMapping("/test")
-    public List<CmUser> test() {
-        CmUserExample cmUserExample = new CmUserExample();
-        List<CmUser> cmUsers = this.userService.selectByExample(cmUserExample);
+    @RequestMapping("/auth")
+    public Response<CmUser> login(@RequestParam String username, @RequestParam String password) {
 
-        return cmUsers;
+        CmUserExample cmUserExample = new CmUserExample();
+        cmUserExample.createCriteria().andUsernameEqualTo(username).andPasswordEqualTo(password);
+
+        CmUser cmUser = this.userService.selectFirstByExample(cmUserExample);
+
+        Response<CmUser> response = new Response<>();
+
+        response.setSuccess(cmUser == null);
+
+        if (cmUser == null) {
+            response.setMessage("用户不存在，或密码错误！");
+        }
+
+        response.setResults(cmUser);
+
+        return response;
     }
 
 }
