bfd75dfc627f51f71fb7a67765cffef80fec5be9
李智慧 <lizhihui0215@gmail.com>
2017-11-04 02:40:02 +0800

    refine code

--- camile-common/src/main/java/com/camile/common/util/CamileShellCallback.java
+++ camile-common/src/main/java/com/camile/common/util/CamileShellCallback.java
@@ -41,110 +41,156 @@ public class CamileShellCallback extends DefaultShellCallback {
         return mergerFile(newCompilationUnit,existingCompilationUnit);
     }
 
-    private boolean containtsFields(List<FieldDeclaration> fields, FieldDeclaration theField) {
-        boolean isContains = false;
-        for (FieldDeclaration field :fields) {
-            String s = field.getVariables().get(0).getName().getIdentifier();
-            String q = theField.getVariables().get(0).getName().getIdentifier();
-            if (s.equals(q)) isContains = true;
-        }
 
-        return isContains;
-    }
 
     private String mergerFile(CompilationUnit newCompilationUnit, CompilationUnit existingCompilationUnit){
-
-        StringBuilder sb = new StringBuilder(newCompilationUnit.getPackageDeclaration().get().toString());
-        newCompilationUnit.removePackageDeclaration();
-
         //合并imports
-        NodeList<ImportDeclaration> imports = newCompilationUnit.getImports();
-        imports.addAll(existingCompilationUnit.getImports());
-        Set<ImportDeclaration> importSet = new HashSet<ImportDeclaration>();
-        importSet.addAll(imports);
+        Set<ImportDeclaration> importSet = new HashSet<>();
+        importSet.addAll(newCompilationUnit.getImports());
+        importSet.addAll(existingCompilationUnit.getImports());
+        newCompilationUnit.setImports(new NodeList<>(importSet));
 
-        NodeList<ImportDeclaration> newImports = new NodeList<>();
-        newImports.addAll(importSet);
-        newCompilationUnit.setImports(newImports);
-        for (ImportDeclaration i:newCompilationUnit.getImports()) {
-            sb.append(i.toString());
-        }
-        newLine(sb);
         NodeList<TypeDeclaration<?>> types = newCompilationUnit.getTypes();
         NodeList<TypeDeclaration<?>> oldTypes = existingCompilationUnit.getTypes();
 
-        for (int i = 0;i<types.size();i++) {
-            //截取Class
-            String classNameInfo = types.get(i).toString().substring(0, types.get(i).toString().indexOf("{")+1);
-            TypeDeclaration<?> typeDeclaration = types.get(i);
+        // 合并 字段方法
+        for (TypeDeclaration typeDeclaration : oldTypes) {
+            List<FieldDeclaration> fields = typeDeclaration.getFields();
+            List<FieldDeclaration> customFields = this.getCustomFields(fields);
+            List<MethodDeclaration> methods = typeDeclaration.getMethods();
+            List<MethodDeclaration> customMethods = this.getCustomMethods(methods);
 
 
-            sb.append(classNameInfo);
-            newLine(sb);
-            newLine(sb);
-            //合并fields
-            List<FieldDeclaration> fields = types.get(i).getFields();
-            List<FieldDeclaration> oldFields = oldTypes.get(i).getFields();
-
-            for (FieldDeclaration f: fields){
-                javaIndent(sb, 1);
-                sb.append(f.toString());
-                newLine(sb);
-                newLine(sb);
+            TypeDeclaration newTypeDeclaration = this.findNewDeclaration(newCompilationUnit, typeDeclaration);
+            for (FieldDeclaration field : customFields) {
+                newTypeDeclaration.addMember(field);
             }
 
-            for (FieldDeclaration f: oldFields){
-                boolean flag = true;
-                for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
-                    if (f.toString().contains(tag)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag){
-                    javaIndent(sb, 1);
-                    sb.append(f.toString());
-                    newLine(sb);
-                    newLine(sb);
-                }
+            for (MethodDeclaration customMethod : customMethods) {
+                newTypeDeclaration.addMember(customMethod);
             }
 
-            //合并methods
-            List<MethodDeclaration> methods = types.get(i).getMethods();
-            List<MethodDeclaration> existingMethods = oldTypes.get(i).getMethods();
-            for (MethodDeclaration f: methods){
-                sb.append(f.toString());
-                newLine(sb);
-                newLine(sb);
-            }
-            for (MethodDeclaration m:existingMethods){
-                boolean flag = true;
-                for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
-                    if (m.toString().contains(tag)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag){
-                    sb.append(m.toString());
-                    newLine(sb);
-                    newLine(sb);
-                }
-            }
 
-            //判断是否有内部类
-            types.get(i).getChildNodes();
-            for (Node n:types.get(i).getChildNodes()){
-                if (n.toString().contains("static class")){
-                    sb.append(n.toString());
-                }
-            }
         }
 
+//        for (int i = 0;i<types.size();i++) {
+//            //截取Class
+//            String classNameInfo = types.get(i).toString().substring(0, types.get(i).toString().indexOf("{")+1);
+//            TypeDeclaration<?> typeDeclaration = types.get(i);
+//
+//
+//            sb.append(classNameInfo);
+//            newLine(sb);
+//            newLine(sb);
+//            //合并fields
+//            List<FieldDeclaration> fields = types.get(i).getFields();
+//            List<FieldDeclaration> oldFields = oldTypes.get(i).getFields();
+//
+//            for (FieldDeclaration f: fields){
+//                javaIndent(sb, 1);
+//                sb.append(f.toString());
+//                newLine(sb);
+//                newLine(sb);
+//            }
+//
+//            for (FieldDeclaration f: oldFields){
+//                boolean flag = true;
+//                for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
+//                    if (f.toString().contains(tag)) {
+//                        flag = false;
+//                        break;
+//                    }
+//                }
+//                if (flag){
+//                    javaIndent(sb, 1);
+//                    sb.append(f.toString());
+//                    newLine(sb);
+//                    newLine(sb);
+//                }
+//            }
+//
+//            //合并methods
+//            List<MethodDeclaration> methods = types.get(i).getMethods();
+//            List<MethodDeclaration> existingMethods = oldTypes.get(i).getMethods();
+//            for (MethodDeclaration f: methods){
+//                sb.append(f.toString());
+//                newLine(sb);
+//                newLine(sb);
+//            }
+//            for (MethodDeclaration m:existingMethods){
+//                boolean flag = true;
+//                for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
+//                    if (m.toString().contains(tag)) {
+//                        flag = false;
+//                        break;
+//                    }
+//                }
+//                if (flag){
+//                    sb.append(m.toString());
+//                    newLine(sb);
+//                    newLine(sb);
+//                }
+//            }
+//
+//            //判断是否有内部类
+//            types.get(i).getChildNodes();
+//            for (Node n:types.get(i).getChildNodes()){
+//                if (n.toString().contains("static class")){
+//                    sb.append(n.toString());
+//                }
+//            }
+//        }
 
 
-        return sb.append(System.getProperty("line.separator")).append("}").toString();
+
+        return newCompilationUnit.toString();
     }
+
+    private List<FieldDeclaration> getCustomFields(List<FieldDeclaration> fields) {
+        ArrayList<FieldDeclaration> fieldDeclarations = new ArrayList<>();
+
+        for (FieldDeclaration field : fields) {
+            boolean add = true;
+            for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
+                if (field.toString().contains(tag)) {
+                    add = false;
+                    break;
+                }
+            }
+
+            if (add) fieldDeclarations.add(field);
+        }
+
+        return fieldDeclarations;
+    }
+
+    private List<MethodDeclaration> getCustomMethods(List<MethodDeclaration> methods) {
+        ArrayList<MethodDeclaration> methodDeclarations = new ArrayList<>();
+
+        for (MethodDeclaration method : methods) {
+            boolean add = true;
+            for (String tag : MergeConstants.OLD_ELEMENT_TAGS) {
+                if (method.toString().contains(tag)) {
+                    add = false;
+                    break;
+                }
+            }
+
+            if (add) methodDeclarations.add(method);
+        }
+
+        return methodDeclarations;
+    }
+
+    private TypeDeclaration findNewDeclaration(CompilationUnit newCompilationUnit, TypeDeclaration typeDeclaration) {
+        for (TypeDeclaration<?> declaration : newCompilationUnit.getTypes()) {
+            if (declaration.getNameAsString().equals(typeDeclaration.getNameAsString())) {
+                return declaration;
+            }
+        }
+        return null;
+    }
+
 
     @Override
     public boolean isMergeSupported() {
